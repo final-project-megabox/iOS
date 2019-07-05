b840eb9fc8066f68148dcd8532fb3c6672f62d2e
chasuyeon <sy9600@naver.com>
2019-07-05 20:40:37 +0900

    first commit

---
+++ MegaBox/ViewControllers/MovieCategoryVC.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCategoryVC.swift
+//  MegaBox
+//
+//  Created by 차수연 on 05/07/2019.
+//  Copyright © 2019 Fury. All rights reserved.
+//
+
+import UIKit
+
+class MovieCategoryVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+  }
+  
+}
