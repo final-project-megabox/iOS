956bb9a545c4f171ddc606ec050847e5ab09a93b
chasuyeon <sy9600@naver.com>
2019-07-16 16:21:13 +0900

    Mypage, LoginPage add

---
+++ MegaBox/ViewControllers/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  MegaBox
+//
+//  Created by 차수연 on 16/07/2019.
+//  Copyright © 2019 Fury. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  let loginTopView = LoginView()
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.addSubview(loginTopView)
+    loginTopView.translatesAutoresizingMaskIntoConstraints = false
+    let guide = view.safeAreaLayoutGuide
+    loginTopView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
+    loginTopView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+    loginTopView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+    loginTopView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+    
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    self.view.endEditing(true)
+  }
+}
