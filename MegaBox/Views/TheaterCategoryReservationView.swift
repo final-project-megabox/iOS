d3d5a07ce3df701be881e50edcb4412c93bfb16d
furydeveloper <furysecu@gmail.com>
2019-08-06 01:35:37 +0900

    190806_염화 선택 중, 지역과 날씨 버튼 뷰 이동 및 애니메이션 재설정

--- MegaBox/Views/TheaterCategoryReservationView.swift
+++ MegaBox/Views/TheaterCategoryReservationView.swift
@@ -9,13 +9,14 @@
 import UIKit
 
 class TheaterCategoryReservationView: UIView {
+  
   private let shared = MovieDataManager.shared
   var delegate: TheaterCategoryReservationViewDelegate?
   
   private var tableViewMovieData = [[ReservationData]]()
   
   let headerView = TheaterCategoryReservationHeaderView()
-  
+
   private var movieTitleIdx: Int = 0
   private var screenArr: [Int] = []
   
@@ -24,7 +25,6 @@ class TheaterCategoryReservationView: UIView {
   
   private var checkTttle: String = ""
   private var checkIdx: Int = 0
-  
   
   private let menuTitleView: UIView = {
     let view = UIView()
@@ -60,6 +60,46 @@ class TheaterCategoryReservationView: UIView {
     return tableView
   }()
   
+  private let buttonView: UIView = {
+    let view = UIView()
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
+  let placeButton: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = #colorLiteral(red: 0.2745098039, green: 0.2862745098, blue: 0.2901960784, alpha: 1)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+    button.titleLabel?.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    button.setImage(#imageLiteral(resourceName: "android_drop_down_arrow"), for: .normal)
+    button.contentHorizontalAlignment = .left
+    button.imageEdgeInsets = UIEdgeInsets(top: 0, left: (UIScreen.main.bounds.maxX / 2) - 40, bottom: 0, right: 0)
+    button.addTarget(self, action: #selector(touchUpPlaceButton), for: .touchUpInside)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+  
+  let dateButton: UIButton = {
+    let button = UIButton()
+    button.backgroundColor = #colorLiteral(red: 0.2745098039, green: 0.2862745098, blue: 0.2901960784, alpha: 1)
+    button.setTitle("7월 17일(수)", for: .normal)
+    button.titleLabel?.font = UIFont.systemFont(ofSize: 15, weight: .medium)
+    button.titleLabel?.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    button.setImage(#imageLiteral(resourceName: "android_drop_down_arrow"), for: .normal)
+    button.contentHorizontalAlignment = .left
+    button.imageEdgeInsets = UIEdgeInsets(top: 0, left: (UIScreen.main.bounds.maxX / 2) - 40, bottom: 0, right: 0)
+    button.addTarget(self, action: #selector(touchUpDateButton), for: .touchUpInside)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+  }()
+  
+  let bgBottomView: UIView = {
+    let view = UIView()
+    view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.3527664812)
+    view.translatesAutoresizingMaskIntoConstraints = false
+    return view
+  }()
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     
@@ -77,6 +117,13 @@ class TheaterCategoryReservationView: UIView {
   }
   
   
+  @objc private func touchUpPlaceButton() {
+    delegate?.touchUpPlaceButton()
+  }
+  
+  @objc private func touchUpDateButton() {
+    print("hello")
+  }
   
   func makeTableViewMovieData() {
     tableViewMovieData = []
@@ -151,6 +198,25 @@ class TheaterCategoryReservationView: UIView {
     theaterTableView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
     theaterTableView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
     theaterTableView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    
+    self.addSubview(buttonView)
+    let topMargin: CGFloat = ((UIScreen.main.bounds.width * 495) / 844)
+    buttonView.topAnchor.constraint(equalTo: self.topAnchor, constant: topMargin).isActive = true
+    buttonView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+    buttonView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+    buttonView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    buttonView.addSubview(placeButton)
+    placeButton.topAnchor.constraint(equalTo: buttonView.topAnchor).isActive = true
+    placeButton.leadingAnchor.constraint(equalTo: buttonView.leadingAnchor).isActive = true
+    placeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    
+    buttonView.addSubview(dateButton)
+    dateButton.topAnchor.constraint(equalTo: buttonView.topAnchor).isActive = true
+    dateButton.leadingAnchor.constraint(equalTo: placeButton.trailingAnchor).isActive = true
+    dateButton.trailingAnchor.constraint(equalTo: buttonView.trailingAnchor).isActive = true
+    dateButton.widthAnchor.constraint(equalTo: placeButton.widthAnchor).isActive = true
+    dateButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
   }
   
   required init?(coder aDecoder: NSCoder) {
@@ -209,13 +275,22 @@ extension TheaterCategoryReservationView: UITableViewDelegate {
   
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
     // 해더뷰 고정 해제
+    
     let scrollHeaderHeight = ((UIScreen.main.bounds.width * 495) / 844) - 50
     if scrollView.contentOffset.y <= scrollHeaderHeight {
       if scrollView.contentOffset.y >= 0 {
         scrollView.contentInset = UIEdgeInsets(top: -scrollView.contentOffset.y, left: 0, bottom: 0, right: 0)
+        placeButton.frame.origin.y = -(scrollView.contentOffset.y)
+        dateButton.frame.origin.y = -(scrollView.contentOffset.y)
+      } else {
+        // 0보다 작으면
+        placeButton.frame.origin.y = -(scrollView.contentOffset.y)
+        dateButton.frame.origin.y = -(scrollView.contentOffset.y)
       }
     } else if scrollView.contentOffset.y > scrollHeaderHeight {
       scrollView.contentInset = UIEdgeInsets(top: -scrollHeaderHeight, left: 0, bottom: 0, right: 0)
+      placeButton.frame.origin.y = -(scrollHeaderHeight)
+      dateButton.frame.origin.y = -(scrollHeaderHeight)
     }
   }
   
@@ -238,5 +313,44 @@ extension TheaterCategoryReservationView: TheaterCategorySectionCellDelegate {
 extension TheaterCategoryReservationView: TheaterCategoryCellDelegate {
   func touchUpMovieTime(_ data: ReservationData) {
    delegate?.sendMovieData(data)
+  }
+}
+
+
+
+extension TheaterCategoryReservationView: UICollectionViewDataSource {
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TheaterCategoryAdCollectionCell.identifier, for: indexPath) as! TheaterCategoryAdCollectionCell
+    if indexPath.row == 0 {
+      cell.adImage.image = #imageLiteral(resourceName: "ad_movie_reservation1")
+    } else if indexPath.row == 1 {
+      cell.adImage.image = #imageLiteral(resourceName: "ad_movie_reservation2")
+    } else if indexPath.row == 2 {
+      cell.adImage.image = #imageLiteral(resourceName: "ad_movie_reservation3")
+    } else if indexPath.row == 3 {
+      cell.adImage.image = #imageLiteral(resourceName: "ad_movie_reservation4")
+    } else if indexPath.row == 4 {
+      cell.adImage.image = #imageLiteral(resourceName: "ad_movie_reservation5")
+    }
+    return cell
+  }
+}
+
+extension TheaterCategoryReservationView: UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let height = (UIScreen.main.bounds.width * 495) / 844
+    return CGSize(width: UIScreen.main.bounds.width, height: height)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 0
   }
 }
